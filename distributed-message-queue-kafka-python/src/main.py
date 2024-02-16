1feb3d2e03fe616e28fa7b5d12f5fc7c3c8b313b
Ayush Anand <aa.21u10425@btech.nitdgp.ac.in>
2024-02-16 18:32:16 +0000

    fix: adds imports for logging

--- distributed-message-queue-kafka-python/src/main.py
+++ distributed-message-queue-kafka-python/src/main.py
@@ -5,6 +5,13 @@ Distributed Message Queue using Kafka
 from fastapi import FastAPI
 from .utils import MessageItem, MessageItems
 from .kafka_utils import kafka_create_message, kafka_poll_message
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG,  # Set the logging level to DEBUG
+                    format='%(asctime)s - %(levelname)s - %(message)s',  # Define the format of log messages
+                    filename='app.log',  # Specify the file to write logs to
+                    filemode='w')  # Set the file mode to 'write' so that it overwrites the existing content
 
 app = FastAPI()
 
