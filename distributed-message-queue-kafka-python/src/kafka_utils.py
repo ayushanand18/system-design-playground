19e2edc2547477108055691156cd4325b03492a6
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2024-02-24 09:23:43 +0530

    feat: use aiokafka instead of blocking synch Kafka

--- distributed-message-queue-kafka-python/src/kafka_utils.py
+++ distributed-message-queue-kafka-python/src/kafka_utils.py
@@ -4,6 +4,7 @@ Kafka Utility Functions
 
 from fastapi import HTTPException
 from kafka import KafkaAdminClient, KafkaProducer, KafkaConsumer
+from aiokafka import AIOKafkaConsumer
 from datetime import datetime
 import sys
 import os
@@ -16,6 +17,8 @@ load_dotenv()
 
 KAFKA_ADDR = os.getenv("KAFKA_ADDR")
 KAFKA_GRP = os.getenv("KAFKA_GRP")
+KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
+KAFKA_TOPIC = 'your_topic'
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_ADDR)
 
@@ -39,13 +42,21 @@ async def kafka_create_message(message: MessageItems):
 
 async def kafka_poll_message(topic: str) -> Optional[MessageItem]:
     """Poll the next message from the queue on the topic"""
-    consumer = KafkaConsumer(topic, bootstrap_servers=KAFKA_ADDR, group_id=KAFKA_GRP)
-    try:
-        message = next(consumer)
-    except StopIteration:
-        raise HTTPException(status_code=404, detail="No messages available in the topic")
-    
-    message_value = message.value.decode('utf-8')
-    message_data = eval(message_value)  # This is a simple way to convert string dictionary to dictionary. Ensure data integrity in production.
+    return MessageItem(await consume_kafka(topic))
 
-    return MessageItem(**message_data)
+async def consume_kafka(topic):
+    consumer = AIOKafkaConsumer(
+        topic,
+        bootstrap_servers=KAFKA_ADDR,
+        group_id=KAFKA_GRP,
+        auto_offset_reset='latest',  # Set to 'latest' to read only latest messages
+        enable_auto_commit=False
+    )
+
+    await consumer.start()
+
+    
+    async for msg in consumer:
+        yield msg.value.decode('utf-8')
+    
+    await consumer.stop()
